298aab34255350e54c399af5c555cb49bd3be0f6
leouemura <leo.uemura.s3@gmail.com>
2020-07-08 02:57:15 -0300

    Login Design Done

---
+++ src/pages/Login/index.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import './styles.css';
+import {FiLogIn} from 'react-icons/fi';
+import {Link, useHistory} from 'react-router-dom';
+
+export default function Login(){
+
+
+
+    return(
+        <div className="content">
+            <div className="text">Login Form</div>
+            <form action="#">
+                <div className="field">
+                    <span className="fas fa-user"></span>
+                    <input type="text" required/>
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div className="field">
+                    <span className="fas fa-lock"></span>
+                    <input type="password" required/>
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div className="forgot-pass"> <a href="#">Forgot Password?</a> </div>
+                <button>Sign In</button>
+                <div className="signup">Not a member? 
+                    <a href="#">SignUp now</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
