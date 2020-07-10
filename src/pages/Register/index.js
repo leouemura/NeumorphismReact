dec6c95c9a3ff325e7b412d22463ffbee7227975
leouemura <leo.uemura.s3@gmail.com>
2020-07-10 01:48:07 -0300

    Added Submit Form Actions

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -11,16 +11,24 @@ export default function Register(){
     const [selectedStock,setSelectedStock] = useState('')
     const [userStocks,setUserStocks] = useState([]);
 
+    const [name,setName] = useState('');
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [email,setEmail] = useState('');
+    const [whatsapp,setWhatsapp] = useState('');
+
+    const history = useHistory();
+
     useEffect(()=>{
         api.get('fstocks').then(ret => {
             setStocks(ret.data);
         })
     },[])
-
+    /*
     useEffect(()=>{
         console.log(userStocks)
     },[userStocks])
-    
+    */
 
     async function handleSelectedStock(event){
         const chosenStock = event.target.value;
@@ -58,41 +66,62 @@ export default function Register(){
         setUserStocks(filteredStock);
     }
 
+    async function handleSubmit(e){
+        e.preventDefault();
+        
+        const stocks = userStocks;
+        const data = {
+            name,
+            username,
+            password,
+            email,
+            whatsapp,
+            stocks
+        };
+
+        console.log(data)
+        
+        const res = await api.post('users', data)
+        alert(res.data.message)
+        if(res.data.message==="Novo usuario cadastrado com sucesso!"){
+            history.push('/')
+        }
+    }
 
     return(
         <div className="content">
             <div className="text">Register Form</div>
-            <form action="#">
+            <form onSubmit={handleSubmit}>
                 <div className="field">
-                    <input type="text" required/>
+                    <input type="text" value={name} onChange={e => setName(e.target.value)} required/>
                     <label>
                         <FaUserCircle id="icon" size={12}/>
                         Name
                     </label>
                 </div>
                 <div className="field">
-                    <input type="text" required/>
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} required/>
                     <label>
                         <FaUser id="icon" size={12}/>
                         Username
                     </label>
                 </div>
                 <div className="field">
-                    <input type="password" required/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} required/>
                     <label>
                         <FaLock id="icon" size={12}/>
                         Password
                     </label>
                 </div>
                 <div className="field">
-                    <input type="email" id="skip_valid" required/>
+                    <input type="email" id="skip_valid" value={email} onChange={e => setEmail(e.target.value)} required/>
                     <label>
                         <FaEnvelope id="icon" size={12}/>
                         Email
                     </label>
                 </div>
                 <div className="field">
-                    <input type="tel" id="skip_valid" required/>
+                    <input type="tel" id="skip_valid" value={whatsapp} onChange={e => setWhatsapp(e.target.value)} required/>
                     <label>
                         <IoLogoWhatsapp id="icon" size={12}/>
                         Whatsapp
